f57153065941f92d69a2435d42e38d6548349a0e
Hone Won Gi <keyone957@naver.com>
2024-10-30 21:08:13 +0900

    10/30 김서방을 찾아라 프로그래머스

---
+++ FindKim.cpp
@@ -0,0 +1,18 @@
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//string solution(vector<string> seoul) {
+//	string answer = "";
+//	int count = 0;
+//	for (int i = 0; i < seoul.size(); i++)
+//	{
+//		if (seoul[i] == "Kim")
+//		{
+//			count = i; break;
+//		}
+//	}
+//	answer = "김서방은 " + to_string(count) + "에 있다";
+//		return answer;
+//}
