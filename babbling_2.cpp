0631ca72e7d1882b2114fba598a0ce315d9299da
Hone Won Gi <keyone957@naver.com>
2024-11-02 23:08:33 +0900

    ?

---
+++ babbling_2.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int solution(vector<string> babbling) {
+//	int answer = 0;
+//	vector<string> canTalk = { "aya","ye","woo","ma" };
+//	
+//	for (int i = 0; i < babbling.size(); i++)
+//	{
+//		//for()
+//	}
+//	return answer; 
+//}
+//int main()
+//{
+//	
+//}
