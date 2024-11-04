612e2487d89c218a92195755e1eb04a1c2607e22
Hone Won Gi <keyone957@naver.com>
2024-11-04 20:59:19 +0900

    11/04 숫자 문자열과 영단어

---
+++ numberToString.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(string s) {
+	unordered_map<string, string> numWords = {
+		{"zero", "0"}, {"one", "1"}, {"two", "2"}, {"three", "3"},
+		{"four", "4"}, {"five", "5"}, {"six", "6"}, {"seven", "7"},
+		{"eight", "8"}, {"nine", "9"}
+	};
+
+	string result = "";  
+	string temp = "";   
+
+	for (char c : s) {
+		if (isdigit(c)) {
+			result += c;
+		}
+		else {
+			temp += c;
+			if (numWords.find(temp) != numWords.end()) {
+				result += numWords[temp];
+				temp = "";
+			}
+		}
+	}
+
+	return stoi(result);
+}
