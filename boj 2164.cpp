ed215026bb2ff8ce65271466a87462bf907a19a5
Hone Won Gi <keyone957@naver.com>
2025-01-07 21:03:35 +0900

    01/07 boj 2164

---
+++ boj 2164.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int num;
+	cin >> num;
+	int count = 1;
+	queue<int> arr;
+	for (int i = 1; i <= num; i++)
+	{
+		arr.push(i);
+	}
+	while (arr.size() > 1)
+	{
+		if (count % 2 != 0)
+		{
+			arr.pop();
+		}
+		else
+		{
+			int swap = arr.front();
+			arr.pop();
+			arr.push(swap);
+		}
+		count++;
+	}
+	cout << arr.front();
+
+}
