eda14a87169089f977497dd943e79d53360d819b
Hone Won Gi <keyone957@naver.com>
2025-02-05 15:43:09 +0900

    02/05 바킹독 배열 문제

---
+++ boj 11399 ATM.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int count;
+	cin >> count;
+	vector<int> numArr(count);
+	for (int i = 0; i < count; i++)
+	{
+		cin >> numArr[i];
+	}
+}
