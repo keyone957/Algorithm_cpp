3823e9b34f9cfeabbaf2d8e98426120a74ec7b25
Hone Won Gi <keyone957@naver.com>
2025-02-11 17:29:12 +0900

    02/11 boj 2178 미로탐색
    bfs 로 거리까지 찾기

---
+++ 코드 작성요령.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	/*/띄어쓰기 받아올때 공백있는 문자열 받아올때 
+	string s;
+	getline(cin, s);
+	cout << s;
+	*/
+}
