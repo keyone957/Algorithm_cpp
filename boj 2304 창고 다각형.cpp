7ae0b459814331542cc2c27d54c43ee1aae0ff18
Hone Won Gi <keyone957@naver.com>
2025-07-15 11:52:16 +0900

    07/15 : boj 1495 기타리스트

---
+++ boj 2304 창고 다각형.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
