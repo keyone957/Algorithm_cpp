50f9be506e96181199daa1210fa7cc3ff6a67f3b
Hone Won Gi <keyone957@naver.com>
2025-07-17 17:05:38 +0900

    07/17 : boj 1946 신입사원

---
+++ boj 1715 카드 정렬하기.cpp
@@ -0,0 +1,35 @@
+//#include <algorithm>
+//#include <deque>
+//#include <functional>
+//#include <iostream>
+//#include <iterator>
+//#include <list>
+//#include <map>
+//#include <queue>
+//#include <set>
+//#include <sstream>
+//#include <stack>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//void func()
+//{
+//    int count;
+//    cin >> count;
+//    for (int i = 0; i < count; i++)
+//    {
+//        int s, m;
+//        cin >> s >> m;
+//
+//    }
+//}
+//int main()
+//{
+//    ios::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+//    int count;
+//    cin >> count;
+//
+//    cout << 'd';
+//}
