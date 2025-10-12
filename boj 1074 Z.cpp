f7b6528746343911d18faf026f1448bbcf8a2e9b
Hone Won Gi <keyone957@naver.com>
2025-10-12 23:04:10 +0900

    10/12 boj 14501 퇴사

---
+++ boj 1074 Z.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include<unordered_map>
+using namespace std;
+int N, r, c;
+void func(int x, int y, int size)
+{
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> r >> c;
+
+}
