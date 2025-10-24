623fd0ffc239af8300f02fa1930f347340a251f2
Hone Won Gi <keyone957@naver.com>
2025-10-24 16:57:42 +0900

    10/24 boj 14284 간선 이어가기 2

---
+++ boj 2668 숫자고르기.cpp
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include<unordered_map>
+#include<math.h>
+using namespace std;
+/*
+
+*/
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
