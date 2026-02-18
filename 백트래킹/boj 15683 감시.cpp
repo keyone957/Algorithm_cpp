71ee193c4b799b45eb5ff06310aae0319413556f
Hone Won Gi <keyone957@naver.com>
2026-02-18 16:59:53 +0900

    02/18 boj 15683 감시

--- 백트래킹/boj 15683 감시.cpp
+++ 백트래킹/boj 15683 감시.cpp
@@ -21,7 +21,6 @@ using namespace std;
 4 : 3방향
 5 : 모든 방향
 6 : 벽
-
 사각지대의 최소 크기 출력
 */
 struct CCTV
@@ -33,10 +32,12 @@ int board[9][9];
 int cctvCnt = 0;
 int answer = 1e9;
 vector<CCTV>cctvs;
-int dx[4] = { 0,0,-1,1 };
-int dy[4] = { -1,1,0,0 };
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+//상 하 좌 우
 void direct(int y, int x, int dir, int temp[9][9])
 {
+    dir %= 4;
     while (true)
     {
         int nx = x + dx[dir];
@@ -53,18 +54,18 @@ void dfs(int count)
 {
     if (count == cctvCnt)
     {
-        int count = 0;
+        int answerArea = 0;
         for (int i = 0; i < n; i++)
         {
             for (int j = 0; j < m; j++)
             {
-                if (board[i][j]==0)
+                if (board[i][j] == 0)
                 {
-                    count++;
+                    answerArea++;
                 }
             }
         }
-        answer = min(answer, count);
+        answer = min(answer, answerArea);
         return;
     }
     int temp[9][9];
@@ -75,12 +76,46 @@ void dfs(int count)
             temp[i][j] = board[i][j];
         }
     }
-
+    //백트래킹 할때 다시 복구해야할때 
+    //한 cctv에 대해서 모든 방향의 경우를 돌아보기 위해 . 
+    //돌리전에 board의 상태를 저장해 놓고 
+    //백트래킹이 끝나면 board상태 복구
     CCTV cur = cctvs[count];
     for (int i = 0; i < 4; i++)
     {
-        direct(cur.y,cur.x,cur.type,temp);
-        dfs(count+1);
+        switch (cur.type)
+        {
+            case 1:
+                direct(cur.y, cur.x, i, board);
+                break;
+            case 2:
+                direct(cur.y, cur.x, i, board);
+                direct(cur.y, cur.x, i + 2, board);
+                break;
+            case 3:
+                direct(cur.y, cur.x, i, board);
+                direct(cur.y, cur.x, i + 3, board);
+                break;
+            case 4:
+                direct(cur.y, cur.x, i, board);
+                direct(cur.y, cur.x, i + 2, board);
+                direct(cur.y, cur.x, i + 3, board);
+                break;
+            case 5:
+                direct(cur.y, cur.x, i, board);
+                direct(cur.y, cur.x, i + 1, board);
+                direct(cur.y, cur.x, i + 2, board);
+                direct(cur.y, cur.x, i + 3, board);
+                break;
+        }
+        dfs(count + 1);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                board[i][j] = temp[i][j];
+            }
+        }//백트래킹 끝나서 복구
     }
 }
 int main()
@@ -108,5 +143,5 @@ int main()
         }
     }
     dfs(0);
-    cout << cctvCnt;
+    cout << answer;
 }
