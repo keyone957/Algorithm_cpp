0258f1be3b248d8dba755e88eaf5153993030bfa
Hone Won Gi <keyone957@naver.com>
2026-03-11 16:58:41 +0900

    03/11 boj 2493 탑

---
+++ 자료구조/boj 2493 탑 (3).cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include<unordered_map>
+#include<math.h>
+#include<cstring>
+using namespace std;
+/*
+각 위치에서 왼쪽으로 레이저를 쏜다 했을 때 자신 보다 높은 곳에 레이저를 송신함
+이제 현재 탑에서 레이저 수신을 받는 위치를 출력
+만일 레이저 신호를 수신하지 않으면 0출력
+
+*/
+int n;
+int tower[500001];
+pair<int, int> tinfo;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> tower[i];
+    }
+    stack<pair<int,int>> s;
+    for (int i = 1; i <= n; i++)
+    {
+        while (!s.empty()&&s.top().first<tower[i])//현재 나보다 작은 타워들은 필요가 없음.
+        {
+            s.pop();
+        }
+        if (s.empty())//만약에 남아있는 나보다 큰 타워가 없으면 수신 불가
+        {
+            cout << 0 << ' ';
+        }
+        else
+        {
+            cout << s.top().second<<' ';
+        }
+        s.push({ tower[i],i });
+        //현재 비교할 타워를 일단 stack에 저장
+        //스택을 사용하여 이제 가장 가까운 내 수신을 받을 타워의 인덱스를 출력할 수 있음
+    }
+}
