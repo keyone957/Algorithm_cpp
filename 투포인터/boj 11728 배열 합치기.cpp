dc0201461cc3b0e6431762a1399e903da981d00d
Hone Won Gi <keyone957@naver.com>
2026-04-27 22:55:36 +0900

    04/27 boj 11728 배열 합치기

---
+++ 투포인터/boj 11728 배열 합치기.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+#include <unordered_set>
+#include<unordered_map>
+#include<math.h>
+#include<cstring>
+using namespace std;
+int n, m;
+vector<int> a;
+vector<int> b;
+vector<int> resultArr;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        a.push_back(num);
+    }
+    for (int j = 0; j < m; j++)
+    {
+        int num;
+        cin >> num;
+        b.push_back(num);
+    }
+    int aIdx = 0;
+    int bIdx = 0;
+    while (aIdx < n && bIdx < m)
+    {
+
+        if (a[aIdx] > b[bIdx])
+        {
+            resultArr.push_back(b[bIdx]);
+            bIdx++;
+        }
+        else
+        {
+            resultArr.push_back(a[aIdx]);
+            aIdx++;
+        }
+    }
+    while (aIdx < n)
+    {
+        resultArr.push_back(a[aIdx]);
+        aIdx++;
+    }
+    while (bIdx < m)
+    {
+        resultArr.push_back(b[bIdx]);
+        bIdx++;
+    }//배열에서 남은것들 없어질때까지 추가
+    for (int i = 0; i < resultArr.size(); i++)
+    {
+        cout << resultArr[i] << ' ';
+    }
+}
